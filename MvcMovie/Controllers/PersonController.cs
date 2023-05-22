81481748fc6a09c9851f26470cf747b06aa356e8
acm19 <acm19@outlook.com>
2023-05-22 17:25:40 -0500

    Agregar archivos de proyecto.

---
+++ MvcMovie/Controllers/PersonController.cs
@@ -0,0 +1,99 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person { PersonId = 1, LastName = "Torrico", Name = "Hugo" });
+            people.Add(new Person { PersonId = 2, LastName = "Torrico", Name = "Juan" });
+            people.Add(new Person { PersonId = 3, LastName = "Torrico", Name = "Jose" });
+            people.Add(new Person { PersonId = 4, LastName = "Torrico", Name = "Miguel" });
+
+            return View(people);
+           
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcMovie/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
